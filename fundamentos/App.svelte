a9a5e516e8e212822688636e07932bdf11250280
Valmir <vmcfilho@gmail.com>
2022-07-29 09:45:27 -0300

    Add files via upload

---
+++ fundamentos/App.svelte
@@ -0,0 +1,12 @@
+<script>
+    import Router from "svelte-spa-router"
+    import exercicio1 from "./exercicio1.svelte"
+    import exercicio2 from "./exercicio2.svelte"
+    import exercicio3 from "./exercicio3.svelte"
+</script>
+
+<Router routes={{
+    "/exercicio1": exercicio1,
+    "/exercicio2": exercicio2,
+    "/exercicio3": exercicio3
+}}></Router>
