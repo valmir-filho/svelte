435143148477123a47c278653d8cd980569a0e7f
Valmir <vmcfilho@gmail.com>
2022-07-29 10:28:26 -0300

    Add files via upload

---
+++ fundamentos/src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+    import Router from "svelte-spa-router"
+    import exercicio1 from "./exercicio1.svelte"
+    import exercicio2 from "./exercicio2.svelte"
+    import exercicio3 from "./exercicio3.svelte"
+    import exercicio4 from "./exercicio4.svelte"
+</script>
+
+<Router routes={{
+    "/exercicio1": exercicio1,
+    "/exercicio2": exercicio2,
+    "/exercicio3": exercicio3,
+    "/exercicio4": exercicio4
+}}></Router>
---
+++ fundamentos/src/exercicio4.svelte
@@ -0,0 +1,6 @@
+<script>
+    let local = "src\\assets\\images\\dance.gif";
+    let name = "Alfonso Ribeiro";
+</script>
+
+<img src={local} alt="{name} dancing!">
