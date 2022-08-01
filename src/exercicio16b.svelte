62ad495a5b227a0f4c7f08bdf525e51043eb2132
Valmir <vmcfilho@gmail.com>
2022-08-01 08:47:23 -0300

    Update exercicio16b.svelte

--- src/exercicio16b.svelte
+++ src/exercicio16b.svelte
@@ -1,9 +1,7 @@
-<!-- O elemento <p> representa um parágrafo. Eles são usados para agrupar conteúdos relacionados de qualquer tipo, como imagens e campos de um
-formulário. -->
-<!-- O elemento <span> é um conteiner genérico em linha para conteúdo fraseado, que não representa nada por natureza. Ele pode ser usado para agrupar
-elementos para fins de estilo (usando os atributos "class" ou "id"), ou para compartilhar valores de atributos como "lang". Ele deve ser usado somente
-quando nenhum outro elemento semântico for apropriado. O <span> é muito parecido com o elemento <div>, entretando a <div> é um elemento de nível de
-bloco enquanto o <span> é um elemento em linha. -->
+<!-- O elemento <p> representa um parágrafo. Eles são usados para agrupar conteúdos relacionados de qualquer tipo, como imagens e campos de um formulário. -->
+<!-- O elemento <span> é um conteiner genérico em linha para conteúdo fraseado, que não representa nada por natureza. Ele pode ser usado para agrupar elementos para fins
+de estilo (usando os atributos "class" ou "id"), ou para compartilhar valores de atributos como "lang". Ele deve ser usado somente quando nenhum outro elemento semântico
+for apropriado. O <span> é muito parecido com o elemento <div>, entretando a <div> é um elemento de nível de bloco enquanto o <span> é um elemento em linha. -->
 
 <script>
     const emojis = {maca: "🍎", banana: "🍌", cenoura: "🥕", donut: "🍩", ovo: "🥚"}
